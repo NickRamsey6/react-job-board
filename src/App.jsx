e7e124b748c5fc3a5849e474b0562fb2219cb7dc
Nick Ramsey <nramseysc@gmail.com>
2024-04-26 23:51:49 -0600

    install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
+import { Route, createBrowserRouter, createRoutesFromElements, Router, RouterProvider } from 'react-router-dom';
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import Navbar from "./components/Navbar";
 import ViewAllJobs from "./components/ViewAllJobs";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={ <h1>My App</h1>} />)
+);
+
 const App = () => {
-  return (
-    <>
-    <Navbar />
-    <Hero  />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />
 };
 
 export default App;
