abd5285d7742da5a8a61e0cbe44fa128cc36f9e8
Nick Ramsey <nramseysc@gmail.com>
2024-04-26 23:58:25 -0600

    create MainLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { Route, createBrowserRouter, createRoutesFromElements, Router, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />)
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}>
+      <Route index element={<HomePage />} />
+    </Route> 
+    )
 );
 
 const App = () => {
