53830cfc6c24955e11d34da13ba7bf783e9e08a8
Nick Ramsey <nramseysc@gmail.com>
2024-04-27 00:02:28 -0600

    setup HomePage with components

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Navbar from '../components/Navbar';
+
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <>
+        <Navbar />
+        <Outlet />
+    </>
   )
 }
 
