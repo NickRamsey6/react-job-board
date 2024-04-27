7a793a86661ac0587a5055a72ebc8520d6ef1142
Nick Ramsey <nramseysc@gmail.com>
2024-04-26 23:54:49 -0600

    create homepage page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import { Route, createBrowserRouter, createRoutesFromElements, Router, RouterProvider } from 'react-router-dom';
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import Navbar from "./components/Navbar";
-import ViewAllJobs from "./components/ViewAllJobs";
+import HomePage from './pages/HomePage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={ <h1>My App</h1>} />)
+  createRoutesFromElements(<Route index element={<HomePage />} />)
 );
 
 const App = () => {
